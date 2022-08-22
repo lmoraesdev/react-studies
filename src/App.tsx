fc44ebc56dbda66781c665f472fc5c43fc8e8da5
lmoraesdev <lbc_moraes@outlook.com>
2022-08-21 23:35:47 -0300

    feat: crianção do components botão e formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Formulario from './components/Formulario';
+
 
 function App() {
   return (
     <div className="App">
-			Hellon World
+			<Formulario />
     </div>
   );
 }
