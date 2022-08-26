0993ff7c79b61d81796b6f17e5c739d050d161c0
lmoraesdev <lbc_moraes@outlook.com>
2022-08-25 23:20:33 -0300

    feat: criando um lista de tarefas reativa

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import style from './Botao.module.scss'
-
 class Botao extends React.Component <{children: React.ReactNode}> {
   render (): React.ReactNode {
     return (
